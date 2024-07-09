76a0316d5c0edded00612285747656a26a2c6d8c
Marcin Zawadzki <mar.zawadzki99@gmail.com>
2024-07-09 12:14:41 +0200

    Created Angular project

---
+++ frontend_test_MZ/src/app/content-block/content-block.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-block',
+  standalone: true,
+  imports: [],
+  templateUrl: './content-block.component.html',
+  styleUrl: './content-block.component.scss'
+})
+export class ContentBlockComponent {
+
+}
